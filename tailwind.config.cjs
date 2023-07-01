d69785ae5f253d3e64f05c5835f8acb5b115c5d2
ShaoyaHUANG <shaoyahuang22@gmail.com>
2023-07-01 22:41:11 +0800

    add: basic routing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      purple: '#bc8bf8',
+      green: '#23f367',
+      pink: '#ca39c4',
+    },
+    minHeight: {
+      10: '10px',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
