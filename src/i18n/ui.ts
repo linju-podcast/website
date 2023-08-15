865cfe950d57e9e73b7bd31cc968a07907d4fda9
ShaoyaHUANG <shaoyahuang22@gmail.com>
2023-08-15 20:38:44 +0800

    add: i18n basics

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const language = {
+  en: 'English',
+  cn: 'Chinese',
+};
+
+export const defaultLang = 'cn';
+
+export const ui = {
+  cn: {
+    'nav.home': '首页',
+    'nav.about': '关于',
+    'nav.podcast': '播客',
+  },
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.podcast': 'Podcast',
+  },
+} as const;
+
+export const showDefaultLang = false;
