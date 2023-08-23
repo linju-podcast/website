e49f5e271a87b91cb0e6e3656c057aa5cc0fba31
ShaoyaHUANG <shaoyahuang22@gmail.com>
2023-08-23 14:38:16 +0800

    add: marquee animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -25,7 +25,48 @@ module.exports = {
     fontFamily: {
       sans: ['LXGW Wenkai', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        first: {
+          '0%': {
+            transform: 'translateY(100%)',
+          },
+          '10%': {
+            transform: 'translateY(0)',
+          },
+          '40%': {
+            transform: 'translateY(0)',
+          },
+          '50%': {
+            transform: 'translateY(-100%)',
+          },
+          '100%': {
+            transform: 'translateY(-100%)',
+          },
+        },
+        second: {
+          '0%': {
+            transform: 'translateY(0)',
+          },
+          '50%': {
+            transform: 'translateY(0)',
+          },
+          '60%': {
+            transform: 'translateY(-100%)',
+          },
+          '90%': {
+            transform: 'translateY(-100%)',
+          },
+          '100%': {
+            transform: 'translateY(-200%)',
+          },
+        },
+      },
+      animation: {
+        first: 'first 5s linear infinite',
+        second: 'second 5s linear infinite',
+      },
+    },
     plugins: [require('@tailwindcss/typography')],
   },
 };
